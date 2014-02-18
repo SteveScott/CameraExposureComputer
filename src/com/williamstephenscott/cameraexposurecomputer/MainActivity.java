5788244d1658a59b2de44570cdc18d3b31beb4fc
SteveScott <stevescott517@gmail.com>
2014-02-18 13:35:23 -0500

    Add Shutter Speed Slider
    added shutter speed slider

--- src/com/williamstephenscott/cameraexposurecomputer/MainActivity.java
+++ src/com/williamstephenscott/cameraexposurecomputer/MainActivity.java
@@ -10,14 +10,39 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	
-	private int[] isoOptions = new int[] {100, 200, 400, 800, 1600, 3200};
 	private int isoValue;
+	private int[] isoOptions = new int[] {100, 200, 400, 800, 1600, 3200};
+	
+	private float shutterSpeedValue;
+	private String shutterSpeedString;
+	private String[] shutterSpeedOptions = new String[] {
+			"4",
+			"2",
+			"1",
+			"1/2",
+			"1/4",
+			"1/8",
+			"1/15",
+			"1/30",
+			"1/60",
+			"1/125",
+			"1/250",
+			"1/500",
+			"1/1000"
+			};
+	
+	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//create isoSeekBar and listen to it
 		SeekBar isoSeekBar = (SeekBar) findViewById(R.id.isoSeekBar);
-		isoSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+		isoSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
+
+		{
 			
 			@Override
 			public void onStopTrackingTouch(SeekBar seekBar) {
@@ -37,10 +62,77 @@ public class MainActivity extends Activity {
 			{
 			
 			isoValue = isoOptions[progress];
-			refresh();
+			//refresh
+			TextView isoValueText = (TextView) findViewById(R.id.isoTextView);
+			isoValueText.setText("" + isoValue );
+			}
+		});
+	//create shutterSpeedSeekbar and listen to it
+		SeekBar shutterSpeedSeekBar = (SeekBar) findViewById(R.id.shutterSpeedSeekBar);
+		shutterSpeedSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) 
+			{
+			float[] shutterSpeedFloats = new float[] 
+						{
+							4f,
+							2f,
+							1f,
+							(1 / 2),
+							(1 / 4),
+							(1 / 8),
+							(1 / 15),
+							(1 / 30),
+							(1 / 60),
+							(1 / 125),
+							(1 / 250),
+							(1 / 500),
+							(1 / 1000)
+						};
+				
+				shutterSpeedString = shutterSpeedOptions[progress];
+				shutterSpeedValue = shutterSpeedFloats[progress]; 
+				
+				//convert strings to floats
+				/*if (shutterSpeedString == "4")
+					{
+						shutterSpeedValue = 4.0f ;
+					}
+				
+				if (shutterSpeedString == "2")
+					{
+						shutterSpeedValue = 2.0f;
+					}
+				
+				if (shutterSpeedString == "1")
+				 	{
+						shutterSpeedValue = 2.0f;
+				 	}
+				 	*/
+				//refresh
+				TextView shutterSpeedValueText = (TextView) findViewById(R.id.shutterSpeedTextView);
+				shutterSpeedValueText.setText(shutterSpeedString);
+				
+			
 			}
 		});
 	}
+	
+		
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,9 +141,5 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	private void refresh()
-	{
-		TextView isoValueText = (TextView) findViewById(R.id.textView1);
-		isoValueText.setText("" + isoValue );
-	}
+	
 }
