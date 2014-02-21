c7bfec7804a31feb0791dc98490a3c5aa3c81a22
SteveScott <stevescott517@gmail.com>
2014-02-20 20:37:53 -0500

    Added Exposure Meter
    The exposure calculator is complete

--- src/com/williamstephenscott/cameraexposurecomputer/MainActivity.java
+++ src/com/williamstephenscott/cameraexposurecomputer/MainActivity.java
@@ -8,12 +8,14 @@ import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
 import android.widget.TextView;
 
+import java.lang.Math;
+
 public class MainActivity extends Activity {
 	
 	private int isoValue;
 	private int[] isoOptions = new int[] {100, 200, 400, 800, 1600, 3200};
 	
-	private float shutterSpeedValue;
+	private double shutterSpeedValue;
 	private String shutterSpeedString;
 	private String[] shutterSpeedOptions = new String[] 
 			{
@@ -31,6 +33,7 @@ public class MainActivity extends Activity {
 			"1/500",
 			"1/1000"
 			};
+	
 	private String evString;
 	private int evValue;
 	private String[] evOptions = new String[]
@@ -56,21 +59,44 @@ public class MainActivity extends Activity {
 			"16 - Sand and Snow"
 			};
 	
-	private float fstopValue;
-	private float[] fstopOptions = new float[]
+	private double fstopValue;
+	private double[] fstopOptions = new double[]
 			{
-				2f,
-				2.8f,
-				4f,
-				5.6f,
-				8f,
-				11f,
-				16f,
-				22f,
-				32f,
-				45f,
-				64f
+				2,
+				2.8,
+				4,
+				5.6,
+				8,
+				11,
+				16,
+				22,
+				32,
+				45,
+				64
 			};
+	
+	private int exposureValue;
+	private double exposureValueDouble;
+	private double cdpm2 =  Math.pow(2, (evValue - 3));
+	
+///method for updating exposure
+	//paste this into code:
+	//updateExposure(fstopValue, shutterSpeedValue, isoValue, cdpm2);
+	private void updateExposure(double fstopValue, double shutterSpeedValue, int isoValue, double cdpm2) 
+	{
+		cdpm2 = Math.pow(2, (evValue - 3));
+	//calculate exposure value
+		double exposure1 = (((12.5 * (Math.pow(fstopValue, 2))) / (shutterSpeedValue * isoValue)) - cdpm2);
+	//convert exposure1 to EV equivalent
+		exposureValueDouble = (-1 * (((Math.log (cdpm2 + exposure1) / (Math.log(2.0))) + 3.0) - ((Math.log(cdpm2) / Math.log(2.0)) + 3.0))); 
+		System.out.println(Math.round(exposureValueDouble));
+		int exposureValueInt = (int) (Math.round(exposureValueDouble));
+	//update exposureTextView		
+		TextView exposureValueText = (TextView) findViewById(R.id.exposureTextView);
+		exposureValueText.setText("" + exposureValueInt);
+		return;
+		//return exposureValueDouble;
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) 
@@ -78,7 +104,7 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		//create isoSeekBar and listen to it
+	//create isoSeekBar and listen to it
 		SeekBar isoSeekBar = (SeekBar) findViewById(R.id.isoSeekBar);
 		isoSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
 
@@ -105,8 +131,10 @@ public class MainActivity extends Activity {
 			//refresh
 			TextView isoValueText = (TextView) findViewById(R.id.isoTextView);
 			isoValueText.setText("" + isoValue );
+			updateExposure(fstopValue, shutterSpeedValue, isoValue, cdpm2);
 			}
 		});
+	
 	//create shutterSpeedSeekBar and listen to it
 		SeekBar shutterSpeedSeekBar = (SeekBar) findViewById(R.id.shutterSpeedSeekBar);
 		shutterSpeedSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() 
@@ -133,16 +161,16 @@ public class MainActivity extends Activity {
 							4f,
 							2f,
 							1f,
-							(1 / 2),
-							(1 / 4),
-							(1 / 8),
-							(1 / 15),
-							(1 / 30),
-							(1 / 60),
-							(1 / 125),
-							(1 / 250),
-							(1 / 500),
-							(1 / 1000)
+							(1f / 2f),
+							(1f / 4f),
+							(1f / 8f),
+							(1f / 15f),
+							(1f / 30f),
+							(1f / 60f),
+							(1f / 125f),
+							(1f / 250f),
+							(1f / 500f),
+							(1f / 1000f)
 						};
 				
 				shutterSpeedString = shutterSpeedOptions[progress];
@@ -151,7 +179,7 @@ public class MainActivity extends Activity {
 				//refresh
 				TextView shutterSpeedValueText = (TextView) findViewById(R.id.shutterSpeedTextView);
 				shutterSpeedValueText.setText(shutterSpeedString);
-				
+				updateExposure(fstopValue, shutterSpeedValue, isoValue, cdpm2);
 			
 			}
 			
@@ -182,9 +210,11 @@ public class MainActivity extends Activity {
 			fstopValue = fstopOptions[progress];
 			TextView fstopTextView = (TextView) findViewById(R.id.fstopTextView);
 			fstopTextView.setText("" + fstopValue);
+			updateExposure(fstopValue, shutterSpeedValue, isoValue, cdpm2);
 			}
 		});
-	//create ev SeekBar
+	
+	//create evSeekBar
 		SeekBar evSeekBar = (SeekBar) findViewById(R.id.evSeekBar);
 		evSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener()
 		{
@@ -196,7 +226,7 @@ public class MainActivity extends Activity {
 			evValue = (progress - 2);	
 			TextView evValueText = (TextView) findViewById(R.id.evTextView);
 			evValueText.setText(evString);
-			
+			updateExposure(fstopValue, shutterSpeedValue, isoValue, cdpm2);
 			};
 
 			@Override
@@ -212,7 +242,10 @@ public class MainActivity extends Activity {
 			}
 			
 		});
+		
+
 	}
+
 	
 		
 
